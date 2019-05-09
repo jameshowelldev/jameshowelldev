a94261beae009a982452e3543d3655da86c192e9
James Howell <me@jameshowell.dev>
2019-05-09 17:47:51 +0100

    Initial commmit

---
+++ web/src/components/header.js
@@ -0,0 +1,40 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import Icon from './icons'
+import { cn } from '../lib/helpers'
+import logo from '../images/logo.svg'
+
+import styles from './header.module.css'
+
+const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
+  <div className={styles.root}>
+    <div className={styles.wrapper}>
+      <h1 className={styles.branding}>
+        <Link to='/' title={siteTitle}><img src={logo} /></Link>
+      </h1>
+
+      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
+        <Icon symbol='hamburger' />
+      </button>
+
+      <nav className={cn(styles.nav, showNav && styles.showNav)}>
+        <ul>
+          <li>
+            <Link to='/about/'>About</Link>
+          </li>
+          <li>
+            <Link to='/projects/'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/blog/'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/contact/'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+)
+
+export default Header
